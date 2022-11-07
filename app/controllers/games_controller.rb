54ecd69c493d885062664467d81ec83377b846d3
Suraj Gopal <surajcancode@gmail.com>
2022-11-07 14:57:14 -0500

    game logic done --- pulling from API, comparing, all 3 scenarios nailed. no scoring yet

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    @grid = []
+    10.times do
+      char = letters[rand(letters.length)]
+      @grid << char
+    end
+  end
+
+  def score
+    @answer = params[:answer]
+    @presented = params[:token].downcase
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    comparison = URI.open(url).read
+
+    match = true
+    chars = @answer.split('')
+    p chars
+    chars.each do |x|
+      unless chars.count(x) <= @presented.count(x)
+        match = false
+        break
+      end
+    end
+
+    check = JSON.parse(comparison)
+    hit = check["found"]
+
+    if !hit
+      @message = "Not a word!"
+    elsif !match
+      @message = "Word not in grid!"
+    else
+      @message = "GREAT JOB!"
+    end
+  end
+end
